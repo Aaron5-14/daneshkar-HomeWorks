b41fb4f366a3ed8a6e17fbbfbd314b0301f4c18c
Ali <a.shirazi996@gmail.com>
2024-10-07 19:25:22 +0330

    Some changes to HW4

--- level2/HW4/main.py
+++ level2/HW4/main.py
@@ -1,6 +1,15 @@
 from users import User
+import os
+import platform
+
+def clear_screen():
+    if platform.system() == "Windows":
+        os.system('cls')
+    else:
+        os.system('clear')
 
 def sign_up_menu():
+    clear_screen()
     username = input("Username: ")
     password = input("Password: ")
     phone_number = input("Phone number: ")
@@ -17,7 +26,7 @@ def sign_up_menu():
         else:
             return    
     else:
-        print(new_user.error())
+        print(new_user.error)
         print("0. Back\n1. Try Again")    
         user_input = input("Input: ")
         if user_input == '1':
@@ -27,15 +36,48 @@ def sign_up_menu():
 
 
 def log_in_menu():
+    clear_screen()
     username = input("Username: ")
     password = input("Password: ")
     user = User(username, password)
     
     if user.log_in():
+        clear_screen()
+        print("Successfully Logged In!\n0. Continue")
+        usr_input = input("Input: ")
+        user_panel(user)
+    else:
+        print(user.error)
+        print("0. Back\n1. Try Again")
+        usr_input = input("Input: ")   
+        if usr_input == '0':
+            return
+        else: 
+            log_in_menu()
+         
         
 
 
+def user_panel(user: User):
+    clear_screen()
+    print("1. Account Information\n2. Edit Profile\n3. Change Password\n4. Log Out")
+    user_input = input("Input: ")
+    if user_input == '1':
+        clear_screen()
+        print(user)
+        print('0. Back') 
+        usr_input = input("Input: ")
+        
+    elif user_input == '2':
+        user_edit_info_menu()
+    elif user_input == '3':
+        #get password and verify using static method
+
+
+
+
 while True:
+    clear_screen()
     print("0. Quit\n1. Sign Up\n2. Log In")
     user_input = input("Input: ")
     if user_input == '0':
--- level2/HW4/users.py
+++ level2/HW4/users.py
@@ -1,14 +1,47 @@
 class User:
     registered_users = {}
     def __init__(self, username, password, phone_number = None):
-        self.username = username
-        self.password = password
-        self.phone_number = phone_number
-        self.logged_in = 0
+        self._username = username
+        self.__password = password
+        self._phone_number = phone_number
+        self._logged_in = False
+        self._error = None        
 
     def register(self):
         pass
 
     @property
     def logged_in(self):
-        return self.logged_in    
+        return self.logged_in  
+
+    @logged_in.setter
+    def logged_in(self, status):
+        self.logged_in = status
+
+    @property
+    def error(self):
+        return self._error      
+      
+    def __str__(self) -> str:
+        return f"Id: {self.id}\nUsername: {self._username}\nPhone Number: {self._phone_number}"
+
+    @property
+    def password(self):
+        return self.__password
+
+    @password.setter
+    def password(self, new_password):
+        if new_password == self.__password:
+            self._error = "UserError: Password Already in Use."
+            return 0
+
+        if len(new_password) < 5:
+            self._error = "UserError: Password needs to have at least 4 characters."
+            return 0
+        
+        self.__password = new_password
+        return True
+    
+    def log_in(clss,a):
+        return clss + a
+
